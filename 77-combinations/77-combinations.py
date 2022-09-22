a3ae6958a30c768a2662b2865eb847eccedfc3e2
shinjuno123 <72008909+shinjuno123@users.noreply.github.com>
2022-09-22 21:29:02 +0900

    Time: 1487 ms (5.04%), Space: 16 MB (51.26%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -1,26 +1,19 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        nums = [i for i in range(1,n+1)]
-        prev_combination = []
-        result = []
+        results = []
+        prev = []
         
-        def dfs(idx,nums):
-            if len(nums) == n - k:
-                result.append(prev_combination.copy())
+        def dfs(elements,start,k):
+            
+            if k == 0:
+                results.append(prev.copy())
                 return
             
-            print(idx)
-            for i in range(idx, len(nums)):
-                current_nums = nums.copy()
-                current_nums.remove(nums[i])
-                
-                prev_combination.append(nums[i])
-                
-                dfs(i,current_nums)
-                prev_combination.pop()
+            for i in range(start, n+1):
+                prev.append(i)
+                dfs(prev, i+1, k-1)
+                prev.pop()
         
+        dfs([],1,k)
         
-        dfs(0,nums)
-        
-        
-        return result
+        return results
