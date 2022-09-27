7da7aaf17d64b145c78548ea2b0c682a04cbf6cc
shinjuno123 <72008909+shinjuno123@users.noreply.github.com>
2022-09-27 15:59:16 +0900

    Time: 63 ms (50.00%), Space: 14 MB (9.33%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.py
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.py
@@ -2,7 +2,7 @@ class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
         for letter in sorted(set(s)):
             suffix = s[s.index(letter):]
-            if set(s) == set(suffix):
+            if set(suffix) == set(s):
                 return letter + self.removeDuplicateLetters(suffix.replace(letter,''))
         
         return ''
