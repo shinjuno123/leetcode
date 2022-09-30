6b4fbe5960d1da2af07b8f3b8be9ee4207db6aa5
shinjuno123 <72008909+shinjuno123@users.noreply.github.com>
2022-09-30 15:59:43 +0900

    Time: 3621 ms (5.00%), Space: 14.2 MB (5.26%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -2,19 +2,26 @@ from collections import defaultdict
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        used = {}
-        max_length = start = 0
+        used = set()
         
-        for index, character in enumerate(s):
-            
-            # when meeting overlapping char
-            if character in used and start <= used[character]:
-                start = used[character] + 1
-            else:
-                max_length = max(max_length, index - start + 1)
+        def findSub(index,sub):
+            if s[index] in used:
+                return ''
             
             
-            used[character] = index
+            if s[index] not in used and index < len(s) -1:
+                used.add(s[index])
+                sub += findSub(index+1, s[index+1])
+            
+                    
+            return sub
         
+        res = 0
+        for i in range(len(s)):
+            a = findSub(i,s[i])
+            # print(a)
+            res = max(res,len(a))
+            used.clear()
+            
         
-        return max_length
+        return res
