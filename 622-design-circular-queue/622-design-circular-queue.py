bb42d7930f3cabb2ee55d8fd40d81ba83456d7fc
shinjuno123 <shinjuno123@naver.com>
2022-10-04 15:56:23 +0900

    Time: 150 ms (39.00%), Space: 14.6 MB (57.02%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.py
+++ 622-design-circular-queue/622-design-circular-queue.py
@@ -1,41 +1,45 @@
 class MyCircularQueue:
-
+    
     def __init__(self, k: int):
         self.cqueue = [None] * k
-        self.k = k
         self.front = 0
         self.rear = 0
-    
-    def enQueue(self,value:int):
-        # if there is space
-        if self.cqueue[self.rear] is None:
+        self.len = 0
+        self.max = k
+
+    def enQueue(self, value: int) -> bool:
+        # if there is space in cqueue
+        if self.len < self.max:
             self.cqueue[self.rear] = value
-            self.rear = (self.rear + 1) % self.k
+            self.rear = (self.rear + 1) % self.max
+            self.len += 1
             return True
-        # if there is no space to fill
-        else:
-            return False
         
-    def deQueue(self):
-        if self.cqueue[self.front] is None:
-            return False
-        else:
-            v = self.cqueue[self.rear]
+        return False
+
+    def deQueue(self) -> bool:
+        # if cqueue's space is full
+        if self.len:
             self.cqueue[self.front] = None
-            self.front = (self.front + 1) % self.k
+            self.front = (self.front + 1) % self.max
+            self.len -= 1
             return True
+        
+        return False
     
-    def Front(self):
-        return -1 if self.cqueue[self.front] is None else self.cqueue[self.front]
-    
-    def Rear(self):
-        return -1 if self.cqueue[self.rear - 1] is None else self.cqueue[self.rear - 1]
-    
-    def isEmpty(self):
-        return self.front == self.rear and self.cqueue[self.front] is None
-    
-    def isFull(self):
-        return self.front == self.rear and self.cqueue[self.front] is not None
+
+    def Front(self) -> int:
+        return self.cqueue[self.front] if self.cqueue[self.front] is not None else -1 
+
+    def Rear(self) -> int:
+        return self.cqueue[self.rear - 1] if self.cqueue[self.rear - 1] is not None else -1 
+
+    def isEmpty(self) -> bool:
+        return True if self.rear == self.front and self.cqueue[self.front] is None else False
+
+    def isFull(self) -> bool:
+        return True if self.rear == self.front and self.cqueue[self.rear] else False
+        
 
 
 # Your MyCircularQueue object will be instantiated and called as such:
