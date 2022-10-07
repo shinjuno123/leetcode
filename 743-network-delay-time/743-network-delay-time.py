d247574f994ad149699c7c0b52b9b879c15c9427
shinjuno123 <72008909+shinjuno123@users.noreply.github.com>
2022-10-07 12:53:59 +0900

    Time: 1224 ms (20.89%), Space: 16.6 MB (32.38%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -6,19 +6,19 @@ class Solution:
         
         for u, v, w in times:
             graph[u].append((v,w))
-            
-        Q = [(0,k)] # (time taken == 0, vertax == k)
-        dist = defaultdict(int)
+        
+        
+        Q = [(0,k)]
+        dist = defaultdict()
         
         while Q:
             time, node = heapq.heappop(Q)
             if node not in dist:
                 dist[node] = time
                 for v, w in graph[node]:
-                    alt = time + w
-                    heapq.heappush(Q,(alt, v))
-        
-        
+                    alt = w + time
+                    heapq.heappush(Q,(alt,v))
+
         if len(dist) == n:
             return max(dist.values())
         
