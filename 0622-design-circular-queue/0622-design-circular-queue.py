3ed6606734f40ca90b15b0fafd0c3e9255ee725b
shinjuno123 <72008909+shinjuno123@users.noreply.github.com>
2022-10-18 15:08:00 +0900

    Time: 150 ms (41.19%), Space: 14.5 MB (57.05%) - LeetHub

--- 0622-design-circular-queue/0622-design-circular-queue.py
+++ 0622-design-circular-queue/0622-design-circular-queue.py
@@ -1,40 +1,42 @@
 class MyCircularQueue:
 
     def __init__(self, k: int):
-        self.cq = [None] * k
+        self.cqueue = [None] * k
         self.front = 0
         self.rear = 0
         self.k = k
+        self.len = 0
 
     def enQueue(self, value: int) -> bool:
-        if self.isFull():
+        if self.len < self.k:
+            self.cqueue[self.rear] = value
+            self.rear = (self.rear + 1) % self.k
+            self.len += 1
+            return True
+        else:
             return False
-        
-        self.cq[self.rear] = value
-        self.rear = (self.rear + 1) % self.k
-        return True
-            
 
     def deQueue(self) -> bool:
-        if self.isEmpty():
+        if self.len > 0:
+            self.cqueue[self.front] = None
+            self.front = (self.front + 1) % self.k
+            self.len -= 1
+            return True
+        else:
             return False
-        
-        self.cq[self.front] = None
-        self.front = (self.front + 1) % self.k
-        return True
 
     def Front(self) -> int:
-        return self.cq[self.front] if self.cq[self.front] is not None else -1
+        return self.cqueue[self.front] if self.cqueue[self.front] is not None else -1 
 
     def Rear(self) -> int:
-        return self.cq[self.rear - 1] if self.cq[self.rear - 1] is not None else -1
+        return self.cqueue[self.rear - 1] if self.cqueue[self.rear - 1] is not None else -1
 
     def isEmpty(self) -> bool:
-        return self.rear == self.front and self.cq[self.front] is None 
+        return self.front == self.rear and self.cqueue[self.front] is None
+        
 
     def isFull(self) -> bool:
-        return self.rear == self.front and self.cq[self.front] is not None
-        
+        return self.front == self.rear and self.cqueue[self.front] is not None
 
 
 # Your MyCircularQueue object will be instantiated and called as such:
