66f68037b17424310caa2d22e180f4ffe1f5d52b
shinjuno123 <72008909+shinjuno123@users.noreply.github.com>
2022-11-04 20:25:24 +0900

    Time: 2416 ms (5.01%), Space: 14.9 MB (41.04%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -3,7 +3,8 @@ class Solution:
         
         for i,v in enumerate(numbers):
             compliment = target - v
-            idx1 = bisect.bisect_left(numbers[i+1:],compliment)
-            if idx1 < len(numbers[i+1:]) and numbers[i + idx1 + 1] == compliment:
+            nums = numbers[i+1:]
+            idx1 = bisect.bisect_left(nums,compliment)
+            if idx1 < len(nums) and numbers[i + idx1 + 1] == compliment:
                 return [i+1, idx1 + i + 2]
         
