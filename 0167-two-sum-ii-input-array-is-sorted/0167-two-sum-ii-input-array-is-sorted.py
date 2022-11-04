358d68219b1b8810aeacd16c5938f53aade27881
shinjuno123 <72008909+shinjuno123@users.noreply.github.com>
2022-11-04 20:24:27 +0900

    Time: 7620 ms (5.01%), Space: 14.6 MB (99.85%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,12 +1,9 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         
-        for i in range(len(numbers)):
-            compliment = target - numbers[i]
+        for i,v in enumerate(numbers):
+            compliment = target - v
             idx1 = bisect.bisect_left(numbers[i+1:],compliment)
-            if idx1 < len(numbers[i+1:]) and numbers[i+1:][idx1] == compliment:
+            if idx1 < len(numbers[i+1:]) and numbers[i + idx1 + 1] == compliment:
                 return [i+1, idx1 + i + 2]
-        
-        
-        return
         
