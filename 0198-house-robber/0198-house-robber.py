9cd254c4ba8e4346c6fba23a834b414000b0dda2
shinjuno123 <72008909+shinjuno123@users.noreply.github.com>
2022-11-26 17:50:01 +0900

    Time: 61 ms (43.24%), Space: 14 MB (19.87%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,15 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
+        table = collections.OrderedDict()
+        def _rob(i):
+            if i < 0:
+                return 0
+            if i in table:
+                return table[i]
+    
+            table[i] = max(_rob(i - 1),_rob(i - 2) + nums[i])
+            
+            return table[i]
         
-        if len(nums) <= 2:
-            return max(nums)
-        
-        dp = collections.OrderedDict()
-        dp[0], dp[1] = nums[0], max(nums[0],nums[1])
-        
-        for i in range(2,len(nums)):
-            dp[i] = max(dp[i-1],dp[i-2] + nums[i])
-        
-        return dp.popitem()[1]
+        return _rob(len(nums) - 1)
