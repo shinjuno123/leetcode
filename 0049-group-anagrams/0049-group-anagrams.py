dd80f01fd2b74bf9cd59ec18f0c74f2bdfad1a30
shinjuno123 <72008909+shinjuno123@users.noreply.github.com>
2022-11-30 19:28:52 +0900

    Time: 208 ms (61.13%), Space: 17.8 MB (63.29%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        table = collections.defaultdict(list)
+        # create a dictionary variable "anagrams" initialized with list at each key
+        anagrams = collections.defaultdict(list)
         
+        # loop over the given list, word by word
         for word in strs:
-            table[''.join(sorted(list(word)))].append(word)
-        return table.values()
+            # in each iteration, sort each word lexicograpghically
+            # set sorted word as a key in anagrams
+            # append original word in the list of the key
+            anagrams[''.join(sorted(word))].append(word)
         
-        
-        
+        # convert anagrams into list and return it
+        return anagrams.values()
