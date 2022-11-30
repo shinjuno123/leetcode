5d58538c65d02ce59d7dba512778d9e8b80c167a
shinjuno123 <72008909+shinjuno123@users.noreply.github.com>
2022-11-30 18:11:25 +0900

    Time: 91 ms (9.59%), Space: 14 MB (37.06%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.py
+++ 0819-most-common-word/0819-most-common-word.py
@@ -1,11 +1,22 @@
 import re
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        p =  re.sub('[^a-z]',' ',paragraph.lower())
-        counter = collections.Counter(p.split()).most_common()
- 
-        for word, count in counter:
-            if not word in banned:
-                return word
+        # change all the letters into lowercase letters
+        paragraph = paragraph.lower()
+        # convert all non-alphabets to a space
+        paragraph = re.sub("[^a-z]"," ",paragraph)
+        # split them by a space.
+        paragraph = paragraph.split()
+        words = []
+        # exclude banned words
+        for word in paragraph:
+            if word not in banned:
+                words.append(word)
+        # use counter function to count number of words occurs
+        paragraph_counter = collections.Counter(words)
         
-        return []
+        # get most common word
+        common_word = paragraph_counter.most_common()[0][0]
+        
+        # return the key
+        return common_word
