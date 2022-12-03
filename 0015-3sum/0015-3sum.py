382d0d09382996cea0d89e5eff223a078256501f
shinjuno123 <72008909+shinjuno123@users.noreply.github.com>
2022-12-03 18:38:58 +0900

    Time: 2747 ms (35.45%), Space: 18 MB (83.50%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,22 +1,30 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # sort nums
         nums.sort()
+        # initialize res with an empty list
         res = []
         
-        for i in range(len(nums) - 2):
-            if i > 0 and nums[i - 1] == nums[i]:
-                continue
+        # for iteration with iterating variable i that is index
+        for i in range(len(nums)):
+            # initialize left with i + 1 and initialize right with the end of the length of nums
             left = i + 1
             right = len(nums) - 1
+            # if i is bigger than 0 and nums[i] and nums[i - 1] are same then continue
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+        
+            # while iteration until left pointer's is same to right pointer's
             while left < right:
-                sum_ = nums[left] + nums[right] + nums[i]
-                
+                # get a sum of nums[i] + nums[left] + nums[right] elements
+                sum_ = nums[i] + nums[left] + nums[right]
+                # if sum_ is bigger than 0, then decrement right
                 if sum_ > 0:
                     right -= 1
-                
+                # if sum_ is smaller than 0, then increment left
                 elif sum_ < 0:
                     left += 1
-                
+                # when both are same, append nums[i], nums[left], nums[right] to res and skip duplicate elements
                 else:
                     res.append([nums[i],nums[left],nums[right]])
                     
@@ -24,10 +32,14 @@ class Solution:
                         left += 1
                     
                     while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
+                        right -= 1 
                     
+                    # increment left and decrement right
                     left += 1
                     right -= 1
         
         
+        
+        # return res
         return res
+                
