aa08f312b8706cbcf16597b15af11394d4df9490
shinjuno123 <72008909+shinjuno123@users.noreply.github.com>
2022-12-05 17:04:34 +0900

    Time: 145 ms (48.28%), Space: 13.9 MB (86.65%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,10 +5,13 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        root = head = ListNode(None)
+        # initialize carry with 0
         carry = 0
+        # initialize root with ListNode(None)
+        root = head = ListNode(None)
         
-        while carry or l1 or l2:
+        # loop through the 2 numbers while l1 or l2 or carry exist
+        while l1 or l2 or carry:
             sum_ = 0
             
             if l1:
@@ -18,12 +21,22 @@ class Solution:
             if l2:
                 sum_ += l2.val
                 l2 = l2.next
-            
+                
             if carry:
                 sum_ += carry
             
-            carry, val = divmod(sum_, 10)
-            head.next = ListNode(val)
+            
+            # in each iteration, add l1 number to l2 number and to carry and devide the result with 10
+            quotient, remainder = divmod(sum_,10)
+            
+            # then remainder is going to be one new result node and quotient will be a carry
+            carry = quotient
+            
+            # splicing root and new result node
+            head.next = ListNode(remainder)
             head = head.next
         
+        
+        
+        # return root.next
         return root.next
