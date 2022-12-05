6d53183a2bae39bf084c96fabda7e6c1450cd108
shinjuno123 <72008909+shinjuno123@users.noreply.github.com>
2022-12-05 17:26:21 +0900

    Time: 65 ms (24.01%), Space: 13.9 MB (65.70%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -5,19 +5,23 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        root = prev = ListNode(None)
+        # initialize root with empty linked list and concatenate it with head
+        root = node = ListNode(None)
         root.next = head
         
-        
-        while head and head.next:
-            a, b = head, head.next
+        start, node = node, node.next
+        # swap 2 adjacent nodes in each iteration until node reaches at the end of the Linked list
+        while node and node.next:
+            a, b = node, node.next
             b.next, a.next = a, b.next
-            prev.next = b
-            prev = prev.next.next
-            head = head.next
+            start.next = b
+            start = start.next.next
+            
+            node = node.next
+            
         
         
+        
+        # O(n)
+        # return head
         return root.next
-            
-            
-            
